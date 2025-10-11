aa4f35cb0c5924e3a34f1bb37ca6b1c6c5e49549
CThanarak <thanaruk.pm@gmail.com>
2025-10-12 03:20:11 +0700

    commit

--- lab9/lab9.c
+++ lab9/lab9.c
@@ -1,63 +1,63 @@
 #include <stdio.h>
 
 int main() {
-    int n, i, j;
+    int n, i, j ;
 
-    printf("Enter N : ");  
+    printf( "Enter N : " );   
     if (scanf("%d", &n) != 1) {
-        printf("ให้กรอกเฉพาะตัวเลขเท่านั้น\n");          
-        return 0;
+        printf( "ให้กรอกเฉพาะตัวเลขเท่านั้น\n" ) ;          
+        return 0 ;
     }// End if
 
-    if (n <= 0) {
-        printf("N ต้องมากกว่า 0\n");   
-        return 0;   
+    if ( n <= 0 ) {
+        printf( "N ต้องมากกว่า 0\n") ;   
+        return 0 ;   
     }// End if
 
-    int a[n];
-    int isprime[n];
+    int a[n] ;
+    int isprime[n] ;
 
-    for (i = 0; i < n; i++) {
-        printf("Enter value[%d] : ", i);
-        if (scanf("%d", &a[i]) != 1) {
-            printf("ให้กรอกเฉพาะตัวเลขเท่านั้น\n");
-            return 0;
+    for ( i = 0; i < n; i++ ) {
+        printf( "Enter value[%d] : ", i ) ;
+        if ( scanf( "%d", &a[i] ) != 1 ) {
+            printf( "ให้กรอกเฉพาะตัวเลขเท่านั้น\n" ) ;
+            return 0 ;
         }// End if
 
-        if (a[i] < 2) {
-            isprime[i] = 0;
+        if ( a[i] < 2 ) {
+            isprime[i] = 0 ;
         } else {
-            for (j = 2; j < a[i]; j++) {
-                if (a[i] % j == 0) {
-                    isprime[i] = 0;
-                    break; 
+            for ( j = 2; j < a[i]; j++ ) {
+                if ( a[i] % j == 0 ) {
+                    isprime[i] = 0 ;
+                    break ; 
                 }// End if
             }// End for
 
-            if (j == a[i]) {
-                isprime[i] = 1;
+            if ( j == a[i] ) {
+                isprime[i] = 1 ;
             }// End if
         }// End else
     }// End for
 
-    printf("Index: ");   
-    for (i = 0; i < n; i++) {
-        printf("%d", i);
-        if (i != n - 1) printf("  ");
+    printf( "Index: ") ;   
+    for ( i = 0; i < n; i++ ) {
+        printf( "%d", i ) ;
+        if ( i != n - 1 ) printf( "  " ) ;
     }// End for
-    printf("\n");
+    printf( "\n" ) ;
 
-    printf("Array: ");                              
-    for (i = 0; i < n; i++) {
-        if (isprime[i] == 1) {
-            printf("%d", a[i]);
+    printf( "Array: " ) ;                              
+    for ( i = 0; i < n; i++ ) {
+        if ( isprime[i] == 1 ) {
+            printf( "%d", a[i] ) ;
         } else {
-            printf("#");
+            printf( "#" ) ;
         }// End if
-        if (i != n - 1) printf("  ");
+        if ( i != n - 1 ) printf( "  " ) ;
     }// End for
 
-    printf("\n");
+    printf( "\n" ) ;
 
-    return 0;
+    return 0 ;
 }// End main
