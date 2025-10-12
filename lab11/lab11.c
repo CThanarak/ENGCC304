c54201861a34783e67c2b5e15a1a62daa0c9abc2
CThanarak <thanaruk.pm@gmail.com>
2025-10-12 22:30:38 +0700

    commit

--- lab11/lab11.c
+++ lab11/lab11.c
@@ -1,45 +1,56 @@
 #include <stdio.h>
+#include <string.h>
+
+int inputNumber();
+int power( int base, int exp ) ;
+int isArmstrong( int num ) ;
+void showResult( int num, int isArm ) ;
+
+int main() {
+    int num = inputNumber() ;
+    int check = isArmstrong(num) ;
+    showResult( num, check ) ;          
+    return 0 ;
+}// End main
+
+int inputNumber() {
+    int n ;
+    printf( "Enter Number: " ) ;
+    scanf( "%d", &n ) ;
+    return n ;
+}// End inputNumber
 
 int power( int base, int exp ) {
     int result = 1 ;
-    for ( int i = 0 ; i < exp ; i++ ) {
+    for (int i = 0 ; i < exp; i++ ) {
         result *= base ;
-    } // End for
+    }
     return result ;
-} // End power
+}// End power
 
 int isArmstrong( int num ) {
-    int original = num, remainder, n = 0 ;
+    char str[20] ;
+    sprintf( str, "%d", num ) ;
+    int n = strlen( str ) ;
+
     int result = 0 ;
+    for ( int i = 0 ; i < n; i++ ) {
+        int digit = str[i] - '0' ;
+        result += power( digit, n ) ;
+    }// End for
 
-    int temp = num ;
+    return result == num ;
+}// End isArmstrong
 
-    while ( temp != 0 ) {    
-        temp /= 10 ;
-        n++ ;
-    } // End while
-
-    temp = num ;
-    while ( temp != 0 ) {
-        remainder = temp % 10 ;
-        result += power( remainder, n ) ;
-        temp /= 10 ;
-    } // End while 
-
-    return result == original ;
-} // End isArmstrong
-
-int main() {
-    int num ;
-
-    printf("Enter Number: ") ;
-    scanf("%d", &num) ;
-
-    if (isArmstrong(num)) {
-        printf("Pass.\n") ;
+void showResult( int num, int isArm ) {
+    if (isArm) {
+        printf( "%d is an Armstrong number.\n", num ) ;
+        printf( "Pass.\n" );
     } else {
-        printf("Not Pass.\n") ;
-    }
+        printf( "%d is not an Armstrong number.\n", num ) ;
+        printf( "Not Pass.\n" ) ;
+    }// End if
+}// End main
 
-    return 0;
-} // End main
+//Flowchart : https://drive.google.com/file/d/1ba1wJe4b_rNVQ4tjV3tJi1p8OcTME4AA/view?usp=sharing
+//Debug : https://docs.google.com/spreadsheets/d/1ZsbDRCG94QgdBYijshNNdR8neTAcYeebOOe0mIxrPtg/edit?usp=sharing
