c5df41a75545f648600cdcecf39fb4bcc84b43e0
CThanarak <thanaruk.pm@gmail.com>
2025-10-12 23:15:36 +0700

    commit

--- lab10/lab10.c
+++ lab10/lab10.c
@@ -1,21 +1,44 @@
 #include <stdio.h>
 #include <string.h>
 
+void toLowerCase( char str[] ) ;
+int isPalindrome( char str[] ) ;
+
 int main() {
-    int num, sum = 0 ;
-    char s[20] ;
-    printf( "Enter Number: " ) ;
-    scanf( "%d", &num ) ;
+    char word[100] ;
 
-    sprintf( s, "%d", num ) ;
-    int n = strlen(s) ;
+    printf( "Enter word:\n" ) ;
+    scanf( "%s", word ) ;
 
-    for ( int i = 0; s[i]; i++ ) {
-        int d = s[i] - '0', p = 1 ;
-        for ( int j = 0; j < n; j++ ) p *= d ;
-        sum += p ;
-    }
+    toLowerCase( word ) ;
 
-    printf( sum == num ? "Pass.\n" : "Not Pass.\n" ) ;
+    if( isPalindrome(word) )
+        printf( "Pass.\n" ) ;
+    else
+        printf( "Not Pass.\n" ) ;
+
     return 0 ;
-}
+}// End main
+
+void toLowerCase( char str[] ) {
+    int i ;
+    for( i = 0; str[i] != '\0'; i++ ) {
+        if( str[i] >= 'A' && str[i] <= 'Z' ) {
+            str[i] = str[i] + 32 ;
+        }// End if
+    }// End for
+}// End toLowerCase
+
+int isPalindrome( char str[] ) {
+    int i, len = strlen( str ) ;
+
+    for( i = 0; i < len / 2; i++ ) {
+        if( str[i] != str[len - i - 1] ) {
+            return 0 ;
+        }// End if
+    }// End for
+    return 1 ;
+}// End isPalindrome
+
+//Flowchart : https://drive.google.com/file/d/1vf-OX_ht8mdRfGrUjoQnCoLpAXZzarO8/view?usp=sharing
+//Debug : https://docs.google.com/spreadsheets/d/1LyPwU0w_RuBO0FEiRQHoB1XJ4CrJyo_DjKDuGc0yf10/edit?usp=sharing
